a318fdf01af3e1206892525ffdca58a59eab2491
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-19 09:34:56 +0530

    added dynamic routes

---
+++ in-mem-db.js
@@ -0,0 +1,6 @@
+const books = [
+	{ id: 1, title: "mufasa", author: "simmba" },
+	{ id: 2, title: "odessey", author: "cnolan" },
+];
+
+module.exports = books;
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const books = require("./in-mem-db");
 
 const app = express();
-
 const PORT = 8000;
+
+// ROUTES
+
+app.get("/books", (req, res) => {
+	res.status(200).json(books);
+});
+
+// dynamic route for individual id book IYKYK
+
+app.get("/books/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	if (isNaN(id)) return res.status(400).json({ error: "its a bad request id" });
+	const book = books.find((e) => e.id === id);
+
+	if (!book) return res.status(404).json({ error: "book not found" });
+
+	return res.json(book);
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
